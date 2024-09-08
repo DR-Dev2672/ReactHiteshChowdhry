f212ede3d379b79f07337a793a40267922fb2b4a
dev <thakurdev2672@gmail.com>
2024-09-08 13:38:20 +0530

    github using api call added

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+  fetch('https://api.github.com/users/Dr-Dev2672')
+  .then(res=>res.json())
+  .then(data=>{setData(data)})
+  },[])
+
   return (
-    <div>Github</div>
+    <div className='text-3xl bg-green-600'>Github followers:{data.followers}
+    <img className='text-center' width={300} src={data.avatar_url} alt="Github picture" />
+    </div>
   )
 }
 
--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+    const {user_id}=useParams()
   return (
-    <div>User:${user_id}</div>
+    <div className='bg-gray-600 text-white text-3xl p-4'>User:{user_id}</div>
   )
 }
 
