69dc839e4320c2969a099dae3c24d811900eaf55
dev <thakurdev2672@gmail.com>
2024-10-18 03:20:35 +0530

    Mega blog building basics requirement

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+   
+
+    <>
+
+      <h1>this is appwrite related project </h1>
+    </>
+  )
+}
+
+export default App
