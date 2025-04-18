ad400ddd6f2bfd0b1ded327301680cbb92e58675
dev <thakurdev2672@gmail.com>
2025-04-18 20:07:07 +0530

    another way to write contexts

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
+import  UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl bg-red-600  flex justify-center p-16 m-16'>Hii this is mini context </h1>
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+      </UserContextProvider>
     </>
   )
 }
